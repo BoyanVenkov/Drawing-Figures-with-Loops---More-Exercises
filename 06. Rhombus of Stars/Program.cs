9d530424432b415df212d3a0534016bb0a164939
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-16 19:05:27 +0300

    Added a New Project

---
+++ 06. Rhombus of Stars/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= n; row++)
+{
+    Console.Write(new string(' ', n - row));
+    Console.Write("*");
+    for (int col = 1; col < row; col++)
+    {
+        Console.Write(" *");
+    }
+    Console.WriteLine();
+}
+
+for (int row = n - 1; row >= 1; row--)
+{
+    Console.Write(new string(' ', n - row));
+    Console.Write("*");
+    for (int col = 1; col < row; col++)
+    {
+        Console.Write(" *");
+    }
+    Console.WriteLine();
+}
