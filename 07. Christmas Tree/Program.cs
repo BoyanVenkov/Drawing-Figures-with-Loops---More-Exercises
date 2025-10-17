565f67143e424d9ace28388f1ab5a0ce1cb9ff9b
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-17 06:55:58 +0300

    Added a New Project

---
+++ 07. Christmas Tree/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= n; i++)
+{
+    for (int j = 1; j <= n - i; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+
+    Console.Write(" | ");
+
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
+}
