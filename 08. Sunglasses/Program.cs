7f4163a40ae5c53821e4af216f398ed98783587b
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-17 19:23:39 +0300

    Added a New Project

---
+++ 08. Sunglasses/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 2 * n; i++)
+{
+    Console.Write("*");
+}
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(" ");
+}
+for (int i = 1; i <= 2 * n; i++)
+{
+    Console.Write("*");
+}
+Console.WriteLine();
+
+for (int row = 1; row <= n - 2; row++)
+{
+    Console.Write("*");
+    for (int i = 1; i <= 2 * n - 2; i++)
+    {
+        Console.Write("/");
+    }
+    Console.Write("*");
+
+    if (row == (n - 1) / 2)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write("|");
+        }
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(" ");
+        }
+    }
+
+    Console.Write("*");
+    for (int i = 1; i <= 2 * n - 2; i++)
+    {
+        Console.Write("/");
+    }
+    Console.Write("*");
+
+    Console.WriteLine();
+}
+
+for (int i = 1; i <= 2 * n; i++)
+{
+    Console.Write("*");
+}
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(" ");
+}
+for (int i = 1; i <= 2 * n; i++)
+{
+    Console.Write("*");
+}
+Console.WriteLine();
