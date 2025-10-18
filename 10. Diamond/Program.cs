d09a89dffa252ac9bbafa99c7a5c06864e2f7b9b
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-18 06:36:01 +0300

    Added a New Project

---
+++ 10. Diamond/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+public class Diamond
+{
+    public static void Main(string[] args)
+    {
+        int n;
+        if (!int.TryParse(Console.ReadLine(), out n) || n < 1 || n > 100)
+        {
+
+        }
+
+        int rows = (n % 2 == 0) ? n - 1 : n;
+        int middleIndex = rows / 2;
+
+        for (int i = 0; i < rows; i++)
+        {
+
+            int leftRight = Math.Abs(i - middleIndex);
+
+            int mid = n - 2 * leftRight - 2;
+
+            StringBuilder line = new StringBuilder();
+
+            line.Append(new string('-', leftRight));
+
+            line.Append('*');
+            if (mid >= 0)
+            {
+                line.Append(new string('-', mid));
+                line.Append('*');
+            }
+
+            line.Append(new string('-', leftRight));
+            Console.WriteLine(line.ToString());
+        }
+    }
+}
